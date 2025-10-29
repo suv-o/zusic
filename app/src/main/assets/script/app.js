e3340a2c6dd14e5f3c1557ef6bdb87ff97d556cc
mrzayen <shubhajit3012@gmail.com>
2025-10-29 15:06:02 +0530

    Web Set-up

--- app/src/main/assets/script/app.js
+++
@@ -1,9 +0,0 @@
-const init = () => {
-  window.app?.stopService();
-  Object.assign(document.body.style, {
-    backgroundColor: "#fff"
-  });
-  window.app?.show();
-};
-init();
-//window.app?.dismiss();
