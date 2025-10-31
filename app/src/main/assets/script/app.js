a8e55404d412b60e1f488ff08aa1707c3e67ee31
mrzayen <shubhajit3012@gmail.com>
2025-10-31 18:51:25 +0530

    Ok

--- app/src/main/assets/script/app.js
+++ app/src/main/assets/script/app.js
@@ -1,19 +1,32 @@
 const comd = false;
 (() => {
-	window.app?.run(comd);
-	if (comd || !document.body) return;
-	const media = window.matchMedia("(prefers-color-scheme: dark)");
-	Object.assign(document.body.style, {
-		margin: "0",
-		padding: "0",
-		fontFamily: "Sans-Serif",
-		color: media.matches ? "#fff" : "#000",
-		backgroundColor: media.matches ? "#f00" : "#f00"
-	});
-	media.addEventListener("change", () => {
-		Object.assign(document.body.style, {
-			color: media.matches ? "#fff" : "#000",
-			backgroundColor: media.matches ? "#f00" : "#f00"
-		});
-	});
+  window.app?.run(comd);
+  if (comd || !document.body) return;
+  const media = window.matchMedia("(prefers-color-scheme: dark)");
+  Object.assign(document.body.style, {
+    margin: "0",
+    padding: "0",
+    fontFamily: "Sans-Serif",
+    color: media.matches ? "#fff" : "#000",
+    backgroundColor: media.matches ? "#000" : "fff"
+  });
+  media.addEventListener("change", () => {
+    Object.assign(document.body.style, {
+      color: media.matches ? "#fff" : "#000",
+      backgroundColor: media.matches ? "#000" : "#fff"
+    });
+  });
+  const container = document.createElement("div");
+  Object.assign(container.style, {
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+    display: "flex",
+    justifyContent: "center",
+    alignItem: "center"
+  });
+  const pre = document.createElement("pre");
+  pre.innerHTML = "Sorry Man!";
+  container.appendChild(pre);
+  document.body.appendChild(container);
 })();
